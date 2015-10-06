69a02cb29efc9dfafd0671a76bf11d93730e3c84
Adam Kelso <kelso.adam@gmail.com>
2015-10-06 09:06:05 -0500

    Initial Commit
    Very beginning of project. Do not use yet.

---
+++ src/CalcuTrait.php
@@ -0,0 +1,122 @@
+<?php namespace adamkelso\DoubleA;
+
+trait CalcuTrait {
+    private $calculated = array();
+    // Assumes there is a private $settable array on the class.
+
+    public function __set($name, $value)
+    {
+        if(array_key_exists($name, $this->settable)) {
+            $this->settable[$name] = trim($value);
+
+            // We empty the calculated values because someone changed
+            // the input values. So, calculations need to be remade.
+            foreach ($this->calculated as $k => $v) {
+                unset($this->calculated[$k]);
+            }
+        }elseif(property_exists($this, 'catch')) {
+            $this->catch[$name] = $value;
+        }else{
+            throw new \Exception('Tried to set unknown property on '.get_class($this).' class: '.$name.' with value '.$value);
+        }
+    }
+
+    public function __get($name)
+    {
+        if(empty($this->reflection)) {
+            $this->reflection = new \ReflectionClass($this);
+        }
+
+        if(array_key_exists($name, $this->calculated))
+        {
+            return $this->calculated[$name];
+        }
+
+        else if(array_key_exists($name, $this->settable) && $this->settable[$name] !== null)
+        {
+            return $this->settable[$name];
+        }
+
+        else if(method_exists($this, $name))
+        {
+        	$params = $this->reflection->getMethod($name)->getParameters();
+        	$args = [];
+        	$missing = [];
+
+        	// Internal IOC container
+        	foreach($params as $p)
+        	{
+        		if(isset($this->{$p->name}) || method_exists($this, $p->name))
+        		{
+        			$args[] = $this->{$p->name};
+        			continue;
+        		}
+
+        		$missing[] = $p->name;
+        	}
+
+        	if(count($missing) > 0)
+        	{
+        		throw new \Exception('The following properties need to be set on the '.__CLASS__.' object before the method '.$name.' can be called: '.implode(', ', $missing).'. ');
+        	}
+
+            $this->calculated[$name] = call_user_func_array([$this, $name], $args);
+
+            if(method_exists($this, 'AfterCalc'))
+            {
+                $this->calculated[$name] = $this->AfterCalc($this->calculated[$name]);
+            }
+
+            return $this->calculated[$name];
+        }
+
+        else{
+            throw new \Exception('Asked for undefined property on '.__CLASS__.' class: '.$name);
+        }
+    }
+
+    // Useful to check from outside the class if a property has been set or calculated already.
+    public function __isset($name)
+    {
+        return (array_key_exists($name, $this->calculated)
+            || (array_key_exists($name, $this->settable) && $this->settable[$name] != null)
+        );
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Check
+    |--------------------------------------------------------------------------
+    |
+    | This method is kind of an internal IOC container, in that it checks that the
+    | requested values have been set. If they haven't and they can be
+    | calculated, they are. Otherwise, an exception is thrown.
+    |
+    */
+    protected function Check(...$args)
+    {
+        $debug = debug_backtrace();
+
+        $method = $debug[1]['function'];
+
+        $missing = array();
+
+        foreach($args as $arg)
+        {
+            if(array_key_exists($arg, $this->settable) && $this->settable[$arg] === null)
+            {
+                array_push($missing, $arg);
+            }
+
+            else if(!array_key_exists($arg, $this->calculated) && method_exists($this, $arg))
+            {
+                $this->calculated[$arg] = $this->$arg();
+            }
+        }
+
+        if(count($missing) > 0)
+        {
+            throw new \Exception('The following properties need to be set on the '.get_class($this).' before the method '.$method.' can be called: '.implode(', ', $missing).'. ');
+        }
+    }
+}
---
+++ src/Enumerable.php
@@ -0,0 +1,42 @@
+<?php namespace adamkelso\DoubleA;
+
+trait Enumerable {
+    public static function all()
+    {
+        $ref = new \ReflectionClass(self::class);
+
+        return $ref->getConstants();
+    }
+
+    public static function keys()
+    {
+        $ref = new \ReflectionClass(self::class);
+
+        return array_keys($ref->getConstants());
+    }
+
+    public static function values()
+    {
+        $ref = new \ReflectionClass(self::class);
+
+        return array_values($ref->getConstants());
+    }
+
+    public static function keyByValue($val)
+    {
+        $ref = new \ReflectionClass(self::class);
+
+        foreach($ref->getConstants() as $key => $value)
+        {
+            if($value == $val)
+            {
+                return $key;
+            }
+        }
+    }
+
+    public static function valueByStringKey($key)
+    {
+        return constant(self::class.'::'.$key);
+    }
+}
